fca897fedd843442fccb9555bd7452e5c5007a2d
Orkhan Ahmadov <orkhan.ahmadov28@gmail.com>
2021-03-22 17:58:46 +0100

    Bcrypt added

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-
+from flask_bcrypt import Bcrypt
 
 app =  Flask(__name__)
 app.config.from_object("config")
 db =  SQLAlchemy(app)
 bcrypt =  Bcrypt(app)
-debugger =  DebugToolbarExtension(app)
+# debugger =  DebugToolbarExtension(app)
 login_manager =  LoginManager(app)
 
