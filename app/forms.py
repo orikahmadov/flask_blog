427208ce9013d99a6d3069d3e7e56da6be3da542
Orkhan Ahmadov <orkhan.ahmadov28@gmail.com>
2021-03-22 18:01:23 +0100

    Email validator added

--- app/forms.py
+++ app/forms.py
@@ -1,3 +1,4 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms.fields.html5 import EmailField, DateField, DateTimeField
