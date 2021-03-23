282d4e79e0adb18ee0e5cebf337b58552e89082c
Orkhan Ahmadov <orkhan.ahmadov28@gmail.com>
2021-03-23 14:47:43 +0100

    File Upload is added to the forms

--- app/routes.py
+++ app/routes.py
@@ -2,12 +2,20 @@ from app import app, db, bcrypt
 from app.forms import RegisterForm
 from flask import render_template, redirect, url_for
 from app.models import User
-
-
+from werkzeug.utils import secure_filename
+import secrets, os
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+
+def save_photo(file):
+    file_extension =  os.path.splitext(file)[-1]
+    changed_name =  secrets.token_hex(8)
+    file_full_name =  changed_name + file_extension
+    return file_full_name
 
 
 
@@ -17,19 +25,29 @@ def index():
 def register():
     form =  RegisterForm()
     if form.validate_on_submit():
-        name =  form.name.data
-        email =  form.email.data
-        birthday = form.birthday.data
-        password  =  form.password.data
-        hashed_password =  bcrypt.generate_password_hash(password).decode('utf-8')
-        nationality =  form.nationality.data
-        accepted =  form.accept_terms.data
-        message =  form.message.data
-        user =  User(name =  name, email =  email, birthday =  birthday, password  =  hashed_password,
-        nationality =  nationality, accept_terms = accepted, message =  message)
-        db.session.add(user)
-        db.session.commit()
-        return "Submitted"
+        if form.photo.data:     #If user wants to upload a photo
+            photo =  form.photo.data
+            filename  =  secure_filename(photo.filename)
+            changed_name =  save_photo(filename)
+            photo.save(os.path.join(app.config["UPLOAD_PATH"], changed_name))
+            user =  User(name =  form.name.data, email =  form.email.data,
+            birthday =  form.birthday.data, password  =  bcrypt.generate_password_hash(form.password.data).decode("utf-8"),
+            nationality =  form.nationality.data, accept_terms = form.accept_terms.data,
+            message =  form.message.data, photo =  changed_name)
+            db.session.add(user)
+            db.session.commit()
+            print("Submitted")
+            return redirect(url_for("index"))
+        else:
+            user = User(name=form.name.data, email=form.email.data,
+                        birthday=form.birthday.data,
+                        password=bcrypt.generate_password_hash(form.password.data).decode("utf-8"),
+                        nationality=form.nationality.data, accept_terms=form.accept_terms.data,
+                        message=form.message.data, photo="default.jpg")
+            db.session.add(user)
+            db.session.commit()
+            print("Submitted without photo")
+            return redirect(url_for("index"))
     return render_template("register.html", form =  form)
 
 
